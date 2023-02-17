ef6c201009774aa26cf4d167f2977d7fdf35e39d
Mahendra <mahendra.chawla26@gmail.com>
2023-02-17 17:47:22 +0530

    Commit Again

--- src/com/maxx/Demo.java
+++ src/com/maxx/Demo.java
@@ -5,6 +5,7 @@ public class Demo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Git with Eclipse");
+		System.out.println("Trying again GIT");
 
 	}
 
